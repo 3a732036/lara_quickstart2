7ac60db613f3a791dfe1052e0cbb182cf34ade4a
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2020-12-11 15:42:10 +0800

    透過 User與Task Model建立 users與tasks資料表之間的關聯

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,10 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+
     /**
      * 這些屬性能被批量賦值。
      *
      * @var array
      */
     protected $fillable = ['name'];
+
+    /**
+     * 取得擁有此任務的使用者。
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
