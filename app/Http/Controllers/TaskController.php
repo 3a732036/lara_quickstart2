e974d129bb00b36f516bb7bfea36c039d3e775b3
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2020-12-11 15:46:18 +0800

    建立三個路由與 TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * 建立一個新的控制器實例。
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+}
