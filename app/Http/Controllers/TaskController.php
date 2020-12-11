bcfd43257d21d8870492fce748c2846c35c4064e
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2020-12-11 16:05:54 +0800

    建立主樣板 master、子樣板 index與共同區塊 errors，並在 TaskController寫上 index與 store的功能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    /**
-     * 建立一個新的控制器實例。
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index',[
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+
     }
 }
